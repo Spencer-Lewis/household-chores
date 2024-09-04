544e7054e3a2fd02ced1f0107493d8f4d5aa7048
Spencer Lewis <spencerlewis987@gmail.com>
2024-09-03 22:58:36 -0400

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { fetchRooms, fetchTasks } from 'api/choresServiceApiClient'
 import NavBar from 'components/NavBar'
-import type { ReactElement } from 'react'
-import { useState, useEffect } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import isChoreDueToday from 'utils/isChoreDueToday'
-import { Room, Task, Chore } from 'types'
-import withNavBar from 'pages/withNavBar'
 import Home from 'pages/Home'
-import Rooms from 'pages/Rooms'
 import Details from 'pages/Room'
+import Rooms from 'pages/Rooms'
 import Tasks from 'pages/Tasks'
-
+import withNavBar from 'pages/withNavBar'
+import type { ReactElement } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+//  Hello
 const WrappedHome = withNavBar(Home)
 const WrappedRooms = withNavBar(Rooms)
 const WrappedDetails = withNavBar(Details)
